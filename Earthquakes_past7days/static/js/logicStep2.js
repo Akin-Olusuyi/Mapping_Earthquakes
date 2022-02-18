a7387ca591e1b5ec9c761c035d57f570f38ead3b
akin-altrio <65419110+akin-altrio@users.noreply.github.com>
2022-02-17 20:47:46 -0500

    Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -28,9 +28,41 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Grabbing our GeoJSON data
+
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feauture) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: "#ffae42",
+        color: "#000000",
+        radius: getRadius(),
+        stroke: true,
+        weight: 0.5
+    };
+}
+
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude * 4;
+}
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
     console.log(data);
-    // Creating a GeoJSON layer with the retrieved data. 
-    L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+
+        // Turn each feature into a circleMarker on the map
+        pointToLayer: function (feauture, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+
+        },
+
+        style: styleInfo
+    }).addTo(map);
+
 });
